f786ab681eef6d005738f2f100794ebab615805d
JulioCValentim <142129412+JulioCValentim@users.noreply.github.com>
2024-01-01 01:19:05 -0300

    Add files via upload

---
+++ projeto Felipao.js
@@ -0,0 +1,22 @@
+let heroi = "Julio Cesar Valentim"
+let quantidadeDeXP = 7000
+console.log("Olá, seja bem vindo, " + heroi + ". Vamos descobrir o seu elo de Jogador, Barbie Manias ;)!")
+
+console.log("--------------------------------------------------------")
+
+let nivelDoJogador = 9800
+
+if(nivelDoJogador === 1000) {
+console.log("Olá! "+ heroi+"Seu nivel é Ferro!")
+} else if(nivelDoJogador <= 2000){
+console.log("Olá! "+ heroi+"Seu nivel é Bronze!")
+
+
+} else if(nivelDoJogador === 2001) {
+console.log("Olá! " +heroi+ " Voce é Prata!")
+}else if(nivelDoJogador <= 5001){
+console.log("Olá! " +heroi+" Voce é Ouro!")
+} else if(nivelDoJogador <= 8001){
+console.log("Olá! " +heroi+ " Voce é Platina!")
+}else if(nivelDoJogador <= 10001){
+console.log("Olá, caro invocador Barbiense! " + heroi+" Atualmente voce esta no Diamante!")}
